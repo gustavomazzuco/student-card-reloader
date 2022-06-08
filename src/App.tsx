74fc24fe0c2c14c24f58ce2a41f589e8f8333f7f
gustavomazzuco <gustavo_mazzuco@hotmail.com>
2022-06-07 21:08:10 -0300

    Ajuste Menu

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ function App() {
 
         <Route path="/login" element={<Login/>}/>
 
+        <Route path="/home" element={<Home/>}/>
+
         <Route path="/" element={<Home/>}/>
 
       </Routes>
